c6510db08cc300a422462d03cfcce1e4139bc53f
Yaniv Gardi <ygardi@codeaurora.org>
2014-07-14 17:56:35 +0300

    phy: fix wrong return value of phy framework APIs
    Currently, a few APIs (init, exit, power_on, power_off,
    suspend, resume) in the phy framework, return a wrong
    value (-ENOTSUPP).
    This change fixes the return value to be 0, to indicate
    there is no actual error.
    
    Change-Id: Ieff05a11738b9d3ec21f171e763c52638de88c8b
    Signed-off-by: Yaniv Gardi <ygardi@codeaurora.org>
    Signed-off-by: erikcas <erikcas1972@gmail.com>

--- drivers/phy/phy-core.c
+++ drivers/phy/phy-core.c
@@ -150,15 +150,30 @@ int phy_init(struct phy *phy)
 {
 	int ret;
 
+	if (!phy->ops->init)
+		return -ENOTSUPP;
+
 	ret = phy_pm_runtime_get_sync(phy);
 	if (ret < 0 && ret != -ENOTSUPP)
 		return ret;
 
+	/*
+	 * If we got here, phy_pm_runtime_get_sync() returned
+	 * either 0 or -ENOTSUPP. In both cases the return value is
+	 * a "non-error" value. Specifically, -ENOTSUPP means no
+	 * phy_pm_runtime_sync support. We should ignore this value
+	 * and continue as usual, since phy_pm_runtime support is not
+	 * mandatory.
+	 */
+	ret = 0;
 	mutex_lock(&phy->mutex);
-	if (phy->init_count++ == 0 && phy->ops->init) {
+
+	if (phy->init_count++ == 0) {
 		ret = phy->ops->init(phy);
 		if (ret < 0) {
 			dev_err(&phy->dev, "phy init failed --> %d\n", ret);
+			/* reverting the init_count since init failed */
+			phy->init_count--;
 			goto out;
 		}
 	}
@@ -174,15 +189,30 @@ int phy_exit(struct phy *phy)
 {
 	int ret;
 
+	if (!phy->ops->exit)
+		return -ENOTSUPP;
+
 	ret = phy_pm_runtime_get_sync(phy);
 	if (ret < 0 && ret != -ENOTSUPP)
 		return ret;
 
+	/*
+	 * If we got here, phy_pm_runtime_get_sync() returned
+	 * either 0 or -ENOTSUPP. In both cases the return value is
+	 * a "non-error" value. Specifically, -ENOTSUPP means no
+	 * phy_pm_runtime_sync support. We should ignore this value
+	 * and continue as usual, since phy_pm_runtime support is not
+	 * mandatory.
+	 */
+	ret = 0;
 	mutex_lock(&phy->mutex);
-	if (--phy->init_count == 0 && phy->ops->exit) {
+
+	if (--phy->init_count == 0) {
 		ret = phy->ops->exit(phy);
 		if (ret < 0) {
 			dev_err(&phy->dev, "phy exit failed --> %d\n", ret);
+			/* reverting the init_count since exit failed */
+			phy->init_count++;
 			goto out;
 		}
 	}
@@ -196,17 +226,33 @@ EXPORT_SYMBOL_GPL(phy_exit);
 
 int phy_power_on(struct phy *phy)
 {
-	int ret = -ENOTSUPP;
+	int ret;
+
+	if (!phy->ops->power_on)
+		return -ENOTSUPP;
 
 	ret = phy_pm_runtime_get_sync(phy);
 	if (ret < 0 && ret != -ENOTSUPP)
 		return ret;
 
+	/*
+	 * If we got here, phy_pm_runtime_get_sync() returned
+	 * either 0 or -ENOTSUPP. In both cases the return value is
+	 * a "non-error" value. Specifically, -ENOTSUPP means no
+	 * phy_pm_runtime_sync support. We should ignore this value
+	 * and continue as usual, since phy_pm_runtime support is not
+	 * mandatory.
+	 */
+	ret = 0;
 	mutex_lock(&phy->mutex);
-	if (phy->power_count++ == 0 && phy->ops->power_on) {
+
+	if (phy->power_count++ == 0) {
 		ret = phy->ops->power_on(phy);
 		if (ret < 0) {
-			dev_err(&phy->dev, "phy poweron failed --> %d\n", ret);
+			dev_err(&phy->dev, "phy power_on failed --> %d\n",
+				ret);
+			/* reverting the power_count since power_on failed */
+			phy->power_count--;
 			goto out;
 		}
 	}
@@ -220,13 +266,20 @@ EXPORT_SYMBOL_GPL(phy_power_on);
 
 int phy_power_off(struct phy *phy)
 {
-	int ret = -ENOTSUPP;
+	int ret = 0;
+
+	if (!phy->ops->power_off)
+		return -ENOTSUPP;
 
 	mutex_lock(&phy->mutex);
-	if (--phy->power_count == 0 && phy->ops->power_off) {
+
+	if (--phy->power_count == 0) {
 		ret =  phy->ops->power_off(phy);
 		if (ret < 0) {
-			dev_err(&phy->dev, "phy poweroff failed --> %d\n", ret);
+			dev_err(&phy->dev, "phy power_off failed --> %d\n",
+				ret);
+			/* reverting the power_count since power_off failed */
+			phy->power_count++;
 			goto out;
 		}
 	}
@@ -238,6 +291,64 @@ out:
 	return ret;
 }
 EXPORT_SYMBOL_GPL(phy_power_off);
+
+int phy_suspend(struct phy *phy)
+{
+	int ret = 0;
+
+	if (!phy->ops->suspend)
+		return -ENOTSUPP;
+
+	mutex_lock(&phy->mutex);
+
+	if (--phy->resume_count == 0) {
+		ret =  phy->ops->suspend(phy);
+		if (ret) {
+			dev_err(&phy->dev, "phy suspend failed --> %d\n", ret);
+			/* reverting the resume_count since suspend failed */
+			phy->resume_count++;
+			goto out;
+		}
+	}
+out:
+	mutex_unlock(&phy->mutex);
+	return ret;
+}
+EXPORT_SYMBOL(phy_suspend);
+
+int phy_resume(struct phy *phy)
+{
+	int ret = 0;
+
+	if (!phy->ops->resume)
+		return -ENOTSUPP;
+
+	mutex_lock(&phy->mutex);
+
+	if (phy->resume_count++ == 0) {
+		ret =  phy->ops->resume(phy);
+		if (ret) {
+			dev_err(&phy->dev, "phy resume failed --> %d\n", ret);
+			/* reverting the resume_count since resume failed */
+			phy->resume_count--;
+			goto out;
+		}
+	}
+out:
+	mutex_unlock(&phy->mutex);
+	return ret;
+}
+EXPORT_SYMBOL(phy_resume);
+
+void phy_advertise_quirks(struct phy *phy)
+{
+	if (phy->ops->advertise_quirks) {
+		mutex_lock(&phy->mutex);
+		phy->ops->advertise_quirks(phy);
+		mutex_unlock(&phy->mutex);
+	}
+}
+EXPORT_SYMBOL(phy_advertise_quirks);
 
 /**
  * of_phy_get() - lookup and obtain a reference to a phy by phandle
